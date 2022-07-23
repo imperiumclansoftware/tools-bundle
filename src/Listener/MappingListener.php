0467de51da7f626d9567965d79bf0e33faaa4fc5
David Dutas <david.dutas@gmail.com>
2022-07-23 11:42:02 +0200

    Ajout du support du schema pour MySQL

---
+++ src/Listener/MappingListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ICS\MediaBundle\EntityListener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver as PdoMysqlDriver;
+use Doctrine\DBAL\Driver\Mysqli\Driver as MysqliDriver;
+
+class MappingListener
+{
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $entityManager = $eventArgs->getEntityManager();
+        $classMetadata = $eventArgs->getClassMetadata();
+        $table = $classMetadata->table;
+        $driver = $eventArgs->getEntityManager()->getConnection()->getDriver()->getDatabasePlatform();
+        $database = $entityManager->getConnection()->getDatabase();
+        
+        if(is_a($driver,MySQLPlatform::class ))
+        {
+            $table['schema']=$database;
+        }
+
+        $classMetadata->setPrimaryTable($table);
+    }
+}
